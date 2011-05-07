27435baa993fea0a30982e2c039e23281d6c0a45
Leif Wickland <leifwickland@gmail.com>
2011-05-07 11:22:14 -0600

    Wrote a scala version of the same.

---
+++ project/build/Project.scala
@@ -0,0 +1,6 @@
+import sbt._
+
+class CodeFellowTestProject(info: ProjectInfo) extends DefaultProject(info) with de.tuxed.codefellow.plugin.CodeFellowPlugin {
+}
+
+
---
+++ src/main/scala/puzzle.scala
@@ -0,0 +1,14 @@
+import scala.io.Source
+import scala.collection.immutable.TreeSet
+
+object puzzle extends Application {
+  val startTime = System.currentTimeMillis
+  val oneLetterElements = "hbcnofpskvyiwu";
+  val twoLetterElements = "he li be ne na mg al si cl ar ca sc ti cr mn fe co ni cu zn ga ge as se br kr rb sr zr nb mo tc ru rh pd ag cd in sn sb te xe cs ba lu hf ta re os ir pt au hg tl pb bi po at rn fr ra lr rf db sg bh hs mt ds rg cn la ce pr nd pm sm eu gd tb dy ho er tm yb ac th pa np pu am cm bk cf es fm md no".split(" ")
+  val colleges = Source.fromFile("college").getLines.foldLeft(new TreeSet[String]) { (tree,line) => tree + line }
+
+  for (college <- colleges;
+       index <- 0 until college.length if oneLetterElements.contains(college(index));
+       newName <- twoLetterElements.map { college.patch(index, _, 1) } if colleges.contains(newName))
+    printf("%s -> %s\n", college, newName)
+}
